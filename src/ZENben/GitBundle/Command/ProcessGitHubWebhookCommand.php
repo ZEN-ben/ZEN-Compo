8b710772f31dd873a7e4758fbc9073bd7058c9fb
Wybren Koelmans <wybrenkoelmans@gmail.com>
2014-07-28 00:48:45 +0200

    forcing error again

--- src/ZENben/GitBundle/Command/ProcessGitHubWebhookCommand.php
+++ src/ZENben/GitBundle/Command/ProcessGitHubWebhookCommand.php
@@ -235,7 +235,7 @@ class ProcessGitHubWebhookCommand extends ContainerAwareCommand
     }
 
     /***
-     * @return string force error -----------------------------------------------------------------------------------------
+     * @return string force error ----------------------------------------------------------------------------------------------------
      */
     protected function phpCs()
     {
