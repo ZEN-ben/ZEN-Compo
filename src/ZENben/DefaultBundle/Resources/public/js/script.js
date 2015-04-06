fd91abd014537a9cbc4ae04231039c46c43fb934
Wybren Koelmans <wybrenkoelmans@gmail.com>
2015-04-06 23:01:59 +0200

    basic monitoring websocket and dash for secure host

---
+++ src/ZENben/DefaultBundle/Resources/public/js/script.js
@@ -0,0 +1,28 @@
+var socket = io(window.location.host+':8080/');
+socket.on('notification', function(msg) {
+    new PNotify(msg);
+});
+
+socket.on('overview', function(overview) {
+    $.each(overview, function(app, isRunning){
+        updateAppState(app, isRunning);
+    });
+});
+
+socket.on('stopped', function(app) {
+    updateAppState(app, false);
+});
+
+socket.on('started', function(app) {
+    updateAppState(app, true);
+});
+
+function updateAppState(app, isRunning) {
+    var $appContainer = $('[data-app="' + app + '"]');
+    $appContainer.toggleClass('running', isRunning);
+    $appContainer.toggleClass('stopped', !isRunning);
+    var $progressBar = $appContainer.find('.progress-bar');
+    $progressBar.toggleClass('active', isRunning);
+    $progressBar.toggleClass('progress-bar-success', isRunning);
+    $progressBar.toggleClass('progress-bar-danger', !isRunning);
+}
